0b2feed2dd40eac9b6a2ace6d0b21db913a75437
NANDO <amnando.bs@gmail.com>
2026-01-05 16:47:55 +0330

    create Post model in posts app

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    STATUS_DRAFT = 'draft'
+    STATUS_PUBLISHED = 'published'
+
+    STATUS_CHOICES = (
+        (STATUS_DRAFT, 'Draft'),
+        (STATUS_PUBLISHED, 'Published'),
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default=STATUS_DRAFT
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title

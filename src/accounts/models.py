305675af279b4290a447132726071059431453d0
NANDO <amnando.bs@gmail.com>
2026-01-05 16:45:44 +0330

    add User model in accounts app

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_ADMIN = 'admin'
+    ROLE_AUTHOR = 'author'
+    ROLE_USER = 'user'
+
+    ROLE_CHOICES = (
+        (ROLE_ADMIN, 'Admin'),
+        (ROLE_AUTHOR, 'Author'),
+        (ROLE_USER, 'User'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=ROLE_USER
+    )
+
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        null=True,
+        blank=True
+    )
+
+    def is_admin(self):
+        return self.role == self.ROLE_ADMIN
+
+    def is_author(self):
+        return self.role == self.ROLE_AUTHOR
+
+    def __str__(self):
+        return self.username

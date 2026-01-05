9cdd901a0f148bfbc9be632564cd9ab731c7280a
NANDO <amnando.bs@gmail.com>
2026-01-05 17:40:14 +0330

    git commit with add 4 serializers in accounts app.

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -5,3 +5,32 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         read_only_fields = ('avatar', 'username', 'bio', 'role', 'email')
 
+class RegisterUserSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True, min_length=8)
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'role')
+
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+                role=validated_data('role', 'user'),
+            )
+            return user
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class UserAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserDetailSerializer(UserSerializer):
+    posts_count = serializers.IntegerField(read_only=True, source='posts.count')
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + ('posts_count',)

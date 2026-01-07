6913a260d89cdfbe47a4f31387997e8838f26d64
NANDO <amnando.bs@gmail.com>
2026-01-07 14:37:53 +0330

    add like model to admin site with advance options.

--- src/likes/admin.py
+++ src/likes/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Like
 # Register your models here.
 
-admin.site.register(Like)
+# admin.site.register(Like)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'created_at')
+    list_filter = ('post', 'user', 'created_at')
+    # search_fields = ['user', 'post']

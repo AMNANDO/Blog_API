d7e9d1aa7f15173586f5183c715f79be34dd1139
NANDO <amnando.bs@gmail.com>
2026-01-07 18:54:46 +0330

    create comment permissions in comments.pemissions file.

--- src/comments/permissions.py
+++ src/comments/permissions.py
@@ -1,1 +1,23 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsCommentOwner(BasePermission):
+    message = 'You don`t own this Comment!!'
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+class IsCommentActive(BasePermission):
+    message = 'can`t show this Comment!!'
+    def has_object_permission(self, request, view, obj):
+        return obj.is_active
+
+class CanCreateComments(BasePermission):
+    message = 'You can`t create comment!!'
+    def has_permission(self, request, view):
+        post = view.get_post()
+        return post.is_active and post.status == 'published'
+
+class CanEditComments(BasePermission):
+    message = 'You can`t edit this comment!!'
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+

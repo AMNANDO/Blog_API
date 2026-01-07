d33262ade92fe428685b0d98ab086e70a14f9df2
NANDO <amnando.bs@gmail.com>
2026-01-07 18:36:57 +0330

    add 7 custom permissions in accounts.permissions file.

--- src/accounts/permissions.py
+++ src/accounts/permissions.py
@@ -1,1 +1,54 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdmin(BasePermission):
+    message = 'You don`t have admin access!!'
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == 'admin'
+        )
+
+class IsAuthor(BasePermission):
+    message = "Your not Author!!"
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == 'author'
+        )
+class IsNormalUser(BasePermission):
+    message = 'Your not User!!'
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == 'user'
+        )
+class CanCreatePost(BasePermission):
+    message = 'You can`t create post!!'
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role in ['admin', 'author']
+        )
+
+class CanModerateContent(BasePermission):
+    message = 'You can`t moderate content!!'
+    def has_permission(self, request, view):
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.role == 'admin'
+        )
+
+class IsSelf(BasePermission):
+    message = 'You don`t own this account!!'
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+class ReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS

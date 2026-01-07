a9a58079b059a5438740265666f3d6509febc6c2
NANDO <amnando.bs@gmail.com>
2026-01-07 14:34:10 +0330

    add comments to admin site with advance options.

--- src/comments/admin.py
+++ src/comments/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Comment
 # Register your models here.
 
-admin.site.register(Comment)
+# admin.site.register(Comment)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'content', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at', 'post', 'author')
+    search_fields = ('content',)

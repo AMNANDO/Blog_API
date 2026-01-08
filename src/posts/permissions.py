ed9a0136f67a06865b20785fa9e739e35cd81a71
NANDO <amnando.bs@gmail.com>
2026-01-08 13:39:11 +0330

    add ispostauthororadmin permission in posts

--- src/posts/permissions.py
+++ src/posts/permissions.py
@@ -1,1 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsPostAuthorOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            obj.author == request.user or
+            request.user.role == 'admin'
+        )

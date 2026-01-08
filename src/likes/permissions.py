6c22ec928ac5635a73b97fcb658afc3c8d76048f
NANDO <amnando.bs@gmail.com>
2026-01-08 14:04:13 +0330

    fix a bug in likepermissions.

--- src/likes/permissions.py
+++ src/likes/permissions.py
@@ -3,4 +3,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class CanLikePost(BasePermission):
     def has_permission(self, request, view):
         post = view.get_post()
-        return not post.likes.filter(user=request.user).exists()
+        return (
+            request.user.is_authenticated and
+            post.is_active and
+            post.status == 'published' and
+            not post.likes.filter(user=request.user).exists()
+        )

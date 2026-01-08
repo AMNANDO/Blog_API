bbf0304694884d63c9a39d1c81d773e213754f4a
NANDO <amnando.bs@gmail.com>
2026-01-08 13:49:31 +0330

    add permission in likePermissions

--- src/likes/permissions.py
+++ src/likes/permissions.py
@@ -1,1 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class CanLikePost(BasePermission):
+    def has_permission(self, request, view):
+        post = view.get_post()
+        return not post.likes.filter(user=request.user).exists()

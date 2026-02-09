ee166c06ef787fd82e05159507d8abed9887fbbc
NANDO <amnando.bs@gmail.com>
2026-02-09 16:01:23 +0330

    do all things in accounts.urls

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,2 +1,6 @@
+from auth_profile_system.src.apps.accounts.urls import urlpatterns
 from .views import *
 from rest_framework.routers import DefaultRouter
+ router = DefaultRouter()
+ router.register()
+ urlpatterns = router.urls

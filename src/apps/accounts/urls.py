27905bb08d1ea871cfdf63e83b5d76239885de7e
NANDO <amnando.bs@gmail.com>
2026-02-16 17:53:15 +0330

    add 3 views in accounts.views file.

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,6 +1,6 @@
 from auth_profile_system.src.apps.accounts.urls import urlpatterns
 from .views import *
 from rest_framework.routers import DefaultRouter
- router = DefaultRouter()
- router.register()
- urlpatterns = router.urls
+router = DefaultRouter()
+router.register()
+urlpatterns = router.urls
--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import (CreateAPIView,
+                                     RetrieveAPIView,
+                                     UpdateAPIView)
+from rest_framework.permissions import (AllowAny,
+                                        IsAuthenticated)
+from .serializers import (RegisterUserSerializer,
+                          UserDetailSerializer,
+                          UpdateUserSerializer)
 
 # Create your views here.
+
+class RegisterUserView(CreateAPIView):
+    serializer_class = RegisterUserSerializer
+    permission_classes = [AllowAny]
+
+class MeView(RetrieveAPIView):
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+class UpdateMeView(UpdateAPIView):
+    serializer_class = UpdateUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user

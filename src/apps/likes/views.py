559260b4b67d1866c8f279bb74341ea972c7d1bc
NANDO <amnando.bs@gmail.com>
2026-02-17 19:14:03 +0330

    import ness things in likes.views file.

--- src/apps/likes/views.py
+++ src/apps/likes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import get_object_or_404
+
+from posts.models import Post
+from .models import Like
+from .serializers import LikeSerializer, LikeCreateSerializer
+from .permissions import CanLikePost
 
 # Create your views here.

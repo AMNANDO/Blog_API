94e8163d8bcba9ec39cd88cf361cd3bd11262cb0
NANDO <amnando.bs@gmail.com>
2026-02-17 19:16:07 +0330

    create get_post function in likes.views file.

--- src/apps/likes/views.py
+++ src/apps/likes/views.py
@@ -11,3 +11,8 @@ from .serializers import LikeSerializer, LikeCreateSerializer
 from .permissions import CanLikePost
 
 # Create your views here.
+
+class PostLikeView(APIView):
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs['post_id'])

f7289e11bbe25757d708d153580509a4befcb2ea
NANDO <amnando.bs@gmail.com>
2026-02-18 17:09:34 +0330

    import ness things in comments.views file.

--- src/apps/comments/views.py
+++ src/apps/comments/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import get_object_or_404
 
+from posts.modles import Post
+from .models import Comment
+from .serializers import (CommentSerializer,
+                          CommentCreateSerializer)
+from .permissions import (CanCreateComments,
+                          CanEditComment,
+                          IsCommentActive,
+                          IsCommentOwner)
 # Create your views here.

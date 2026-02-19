5e60a7170f2218cbceac5638f122a9215c570877
NANDO <amnando.bs@gmail.com>
2026-02-19 14:56:37 +0330

    set url pathes for posts.urls .

--- src/apps/posts/urls.py
+++ src/apps/posts/urls.py
@@ -1,4 +1,8 @@
 from .views import PostViewSet
 from rest_framework.routers import DefaultRouter
+
+app_name = "posts"
+
 router = DefaultRouter()
-router.register()
+router.register(r'', PostViewSet, basename='post')
+urlpatterns = router.urls

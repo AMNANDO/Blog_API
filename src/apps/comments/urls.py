0209bde5debc1606fcfadc19f33dc9dad9da969c
NANDO <amnando.bs@gmail.com>
2026-02-19 15:00:15 +0330

    set url pathes for comments.urls .

--- src/apps/comments/urls.py
+++ src/apps/comments/urls.py
@@ -1,3 +1,10 @@
 from .views import PostCommentView, CommentDetailView
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+
+app_name = "comments"
+
+urlpatterns = [
+    path("posts/<int:psot_id>/comments/", PostCommentView.as_view(), name='post-comments'),
+    path("<int:pk>", CommentDetailView.as_view(), name='comment-detail'),
+]

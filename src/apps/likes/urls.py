5aa0eca156a8b8e4fad2748e22392b27d8ed3143
NANDO <amnando.bs@gmail.com>
2026-02-19 15:02:49 +0330

    set url pathes for likes.urls .

--- src/apps/likes/urls.py
+++ src/apps/likes/urls.py
@@ -1,5 +1,10 @@
 from .views import PostLikeView, PostLikesListView
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register()
+
+app_name = 'likes'
+
+urlpatterns = [
+    path("posts/<int:psot_id>/like/", PostLikeView.as_view(), name='post-like'),
+    path("post/<int:pk>/likes/", PostLikesListView.as_view(), name='post-likes'),
+]
